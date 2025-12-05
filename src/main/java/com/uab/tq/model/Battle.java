e5b58cb2daf1ce9de9af27367e9bd080ca6b075d
xavisrd <xavi.reche@gmail.com>
2025-12-05 09:10:55 +0100

    feat: determineTurnOrder (TDD - GREEN)

--- src/main/java/com/uab/tq/model/Battle.java
+++ src/main/java/com/uab/tq/model/Battle.java
@@ -26,4 +26,23 @@ public class Battle {
     public Character getCharacter2() {
         return character2;
     }
+    
+    /**
+     * Determina qui ataca primer basant-se en la velocitat
+     * 
+     * @return El personatge que ataca primer
+     */
+    public Character determineTurnOrder() {
+        int speed1 = character1.getSpeed();
+        int speed2 = character2.getSpeed();
+        
+        if (speed1 > speed2) {
+            return character1;
+        } else if (speed1 < speed2) {
+            return character2;
+        } else {
+            // Mateixa velocitat: decisió aleatòria
+            return Math.random() < 0.5 ? character1 : character2;
+        }
+    }
 }
